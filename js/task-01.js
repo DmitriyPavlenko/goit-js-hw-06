16bf7e6f96f79eb9fc4e20f40deb9711253dd6ea
Dmitriy Pavlenko <pavlenko.d.o@gmail.com>
2022-10-06 14:52:35 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 const countCat = document.querySelectorAll("#categories .item");
 console.log(`Count categories: ${countCat.length}`);
-
+console.log("");
 countCat.forEach((category) => {
   const titleOfCategory = category.querySelector("h2").textContent;
   const numberOfElements = category.querySelector("ul").children.length;
